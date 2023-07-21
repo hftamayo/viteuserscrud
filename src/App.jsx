802b2c749ee6d27bf903d5abaea685c57e4ccba1
hftamayo <hftamayo@gmail.com>
2023-07-21 12:59:44 -0600

    adding title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import UserList from "./features/users/UserList";
 
 function App() {
   return (
-    <div>
+    <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+      <h1 className="text-center font-bold text-2xl text-gray-700">
+        User's CRUD
+      </h1>
       <UserList />
     </div>
   );
