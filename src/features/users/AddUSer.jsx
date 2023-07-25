3eaab72b18bf93fc238a9793c0638abba3dcca3f
hftamayo <hftamayo@gmail.com>
2023-07-25 12:15:46 -0600

    data handling part 3

--- src/features/users/AddUSer.jsx
+++ src/features/users/AddUSer.jsx
@@ -1,15 +1,18 @@
 import TextField from "../../components/TextField";
 import Button from "../../components/Button";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddUSer = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     name: "",
     email: "",
   });
 
   const handleAddUser = () => {
-    console.log(values);
+    setValues({ name: "", email: "" });
+    navigate("/view-users");
   };
 
   return (
@@ -17,14 +20,14 @@ const AddUSer = () => {
       <TextField
         label="Name"
         value={values.name}
-        onChange={(e) => setValues({ ...values, name: e.target.value })}                
+        onChange={(e) => setValues({ ...values, name: e.target.value })}
         inputProps={{ type: "text", placeholder: "John Doe" }}
       />
       <br />
       <TextField
         label="Email"
         value={values.email}
-        onChange={(e) => setValues({ ...values, email: e.target.value })}        
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
         inputProps={{ type: "email", placeholder: "john@doe.com" }}
       />
       <Button onClick={handleAddUser}>Submit</Button>
