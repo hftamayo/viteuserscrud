be55ce773042577cb853da39f328888fe5ad11e5
hftamayo <hftamayo@gmail.com>
2023-07-25 12:11:09 -0600

    handling data part2

--- src/features/users/AddUSer.jsx
+++ src/features/users/AddUSer.jsx
@@ -1,5 +1,6 @@
 import TextField from "../../components/TextField";
 import Button from "../../components/Button";
+import { useState } from "react";
 
 const AddUSer = () => {
   const [values, setValues] = useState({
@@ -7,22 +8,23 @@ const AddUSer = () => {
     email: "",
   });
 
-  const handleAddUser =() => {
-    
-  }
+  const handleAddUser = () => {
+    console.log(values);
+  };
+
   return (
     <div className="mt-10 max-w-xl mx-auto">
       <TextField
         label="Name"
         value={values.name}
-        onChange={(e) => setValues({ ...values, name: e.target.value })}
+        onChange={(e) => setValues({ ...values, name: e.target.value })}                
         inputProps={{ type: "text", placeholder: "John Doe" }}
       />
       <br />
       <TextField
         label="Email"
         value={values.email}
-        onChange={(e) => setValues({ ...values, email: e.target.value })}
+        onChange={(e) => setValues({ ...values, email: e.target.value })}        
         inputProps={{ type: "email", placeholder: "john@doe.com" }}
       />
       <Button onClick={handleAddUser}>Submit</Button>
